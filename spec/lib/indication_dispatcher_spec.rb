d10ed0000ed031d9f8386cd9d06df6c5fe22fda0
Joe Fiorini <joe@faithfulgeek.org>
2010-10-31 02:24:51 -0400

    Remove useless spec

--- spec/lib/indication_dispatcher_spec.rb
+++ spec/lib/indication_dispatcher_spec.rb
@@ -23,19 +23,4 @@ describe IndicationDispatcher do
   its(:dispatch_instance) { should be_a(IndicationDispatchers::MyDispatch) }
 
 
-  describe IndicationDispatchers::MyDispatch do
-
-
-    before(:all) do
-      @order = OpenStruct.new
-    end
-
-    let(:dispatcher) { IndicationDispatcher.new(:my_dispatch, @order) }
-    subject { dispatcher.dispatch_instance }
-
-    its(:order) { should == @order }
-
-  end
-
-
 end
